b1062705978efee441bff331f9102f75c1f9b828
Pablo Riera <pablo.riera@gmail.com>
2020-07-10 22:16:40 -0300

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
-// Vue.config.devtools = true;
-// window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue
+Vue.config.devtools = true;
+window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue
 
 Vue.prototype.$midiBus = new Vue(); // Global event bus
 
